7a18fbd124a2fecf245970ae3ef9fca7a874617b
Sherif <sherif123samir@hotmail.com>
2026-04-17 15:44:32 +0200

    added model file (empty) main file is now parsing the step argument

--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 import sys
 import os
 from dotenv import load_dotenv
-import importlib.metadata
+#import importlib.metadata
+import argparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 from data_prep.Normalizer import Normalizer
 
-if __name__ == "__main__":
-    #version = importlib.metadata.version('python-dotenv')
-    #print(f"python-dotenv version: {version}")
+def parse_step():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--step', type=str, help='Step name')
+    args = parser.parse_args()
+    if args.step:
+        pass
+    else:
+        print("No step provided.")
+        quit()
+    return args.step
 
-    load_dotenv("config/.env")
+def data_prep():
     normalizer = Normalizer()
     normalizer.load(os.environ.get("TRAIN_RAW_DIR"))
     print(f"Status: {normalizer.status}")
@@ -19,3 +27,23 @@ if __name__ == "__main__":
         print("Book list is empty, Exiting...")
         quit()
     normalizer.normalize()
+
+if __name__ == "__main__":
+    #version = importlib.metadata.version('python-dotenv')
+    #print(f"python-dotenv version: {version}")
+    step=parse_step()
+    load_dotenv("config/.env")
+    if(step == "dataprep"):
+        data_prep()
+        pass
+    elif(step == "model"):
+        pass
+    elif(step == "inference"):
+        pass
+    elif(step == "all"):
+        data_prep()
+        pass
+    else:
+        print("Wrong step provided.")
+        quit()
+
---
+++ src/model/ngram_model.py
@@ -0,0 +1,21 @@
+ import os
+ 
+ class NGramModel:
+
+     def build_vocab(self, token_file):
+         pass
+
+    def build_counts_and_probabilities(self):
+        pass
+
+    def lookup(self):
+        pass
+
+    def save_model(self, model_path):
+        pass
+
+    def save_vocab(vocab_path):
+        pass
+
+    def load(self, vocab_path):
+        pass
